b98aa1ee9b77f25536a8a151fb12f3bcbb468f49
wangtianhao <wangtianhao-6>
2021-10-10 15:18:40 +0800

    第三次 commit

--- src/com/java1/Hello.java
+++ src/com/java1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("第三次");
+        System.out.println("fff");
     }
 }
